f06d582a102b0e7c86bc848ccc7ef349eb2371da
Abdullah Hassan <abdullahzen25@gmail.com>
2022-08-29 16:43:16 +0200

    index and show are done in post controllers

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  validates :headline, presence: true
+  validates :description, presence: true
+  validates :tag, inclusion: { in: ['Visa', 'Accommodation', 'Education', 'Work', 'Taxes', 'Registration', 'Translation', 'Insurance'] }
 end
